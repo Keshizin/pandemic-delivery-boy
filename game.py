d0bb1fe14477e678b6c371bcb251c5367058d1f5
Keshizin <fab.takeshi@gmail.com>
2020-10-12 15:48:27 -0300

    Inclusão do personagem do jogador (placeholder)

--- game.py
+++ game.py
@@ -6,13 +6,23 @@
 import pygame, sys, os
 from pygame.locals import *
 
+COLOR_AQUA_BLUE = (146,244,255)
+
 # initialize all imported pygame modules
 pygame.init()
 
 # my game screen resolution
-GAME_WIDTH_SCREEN = 640
-GAME_HEIGHT_SCREEN = 480
+GAME_WIDTH_SCREEN = 1100
+GAME_HEIGHT_SCREEN = 800
 
+player = pygame.image.load('assets/player.png')
+player_pos = [0, 0]
+
+keys = [False, False, False, False]
+
+# -----------------------------------------------------------------------------
+#  MAIN
+# -----------------------------------------------------------------------------
 def main():
 	print("Pandemic Delivery Game")
 
@@ -27,6 +37,9 @@ def main():
 	# initialize a window or screen for display - SURFACE CREATION HERE!
 	screen = pygame.display.set_mode((GAME_WIDTH_SCREEN, GAME_HEIGHT_SCREEN), 0, 32)
 
+	# -------------------------------------------------------------------------
+	#  GAME LOOP
+	# -------------------------------------------------------------------------
 	while True:
 		# get events from the queue
 		for event in pygame.event.get():
@@ -36,6 +49,59 @@ def main():
 				pygame.quit()
 				sys.exit()
 
+			# MOUSE INPUT
+			if event.type == MOUSEBUTTONDOWN:
+				print("MOUSE DOWN")
+
+			if event.type == MOUSEMOTION:
+				print("MOUSE MOTION")
+
+			if event.type == MOUSEBUTTONUP:
+				print("MOUSE UP")
+
+			# KEYBOARD INPUT (PRESS DOWN)
+			if event.type == KEYDOWN:
+				if event.key == K_LEFT:
+					keys[0] = True
+				if event.key == K_RIGHT:
+					keys[1] = True
+				if event.key == K_UP:
+					keys[2] = True
+				if event.key == K_DOWN:
+					keys[3] = True
+
+			# KEYBOARD INPUT (RELEASE)
+			if event.type == KEYUP:
+				if event.key == K_LEFT:
+					keys[0] = False
+				if event.key == K_RIGHT:
+					keys[1] = False
+				if event.key == K_UP:
+					keys[2] = False
+				if event.key == K_DOWN:
+					keys[3] = False
+
+		# ---------------------------------------------------------------------
+		#  PLAYER INPUT
+		# ---------------------------------------------------------------------
+		if keys[0]:
+			player_pos[0] -= 1
+		
+		if keys[1]:
+			player_pos[0] += 1
+		
+		if keys[2]:
+			player_pos[1] -= 1
+		
+		if keys[3]:
+			player_pos[1] += 1	
+
+		# ---------------------------------------------------------------------
+		#  CLEAR SCREEN
+		# ---------------------------------------------------------------------
+		screen.fill(COLOR_AQUA_BLUE)
+
+		screen.blit(player, player_pos)
 
 		# update portions of the screen for software displays
 		# pygame.display.update()
