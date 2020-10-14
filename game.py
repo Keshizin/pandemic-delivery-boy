6ccc2dc4cb276b1a489f0c86b3c6ffd61bf0b8a8
Keshizin <fab.takeshi@gmail.com>
2020-10-14 10:08:38 -0300

    Testing momentum in Y axis

--- game.py
+++ game.py
@@ -17,8 +17,13 @@ GAME_HEIGHT_SCREEN = 800
 
 player = pygame.image.load('assets/player.png')
 player_pos = [0, 0]
+player_y_momentum = 0
 
+# keys for arrows (LEFT, RIGHT, TOP, BOTTOM)
 keys = [False, False, False, False]
+
+# font = pygame.font.Font('freesansbold.ttf', 15)
+# text = font.render('Seu texto aqui', True, RED, WHITE)
 
 # -----------------------------------------------------------------------------
 #  MAIN
@@ -26,7 +31,10 @@ keys = [False, False, False, False]
 def main():
 	print("Pandemic Delivery Game")
 
-	# clock = pygame.time.Clock()
+	global player_y_momentum
+
+	# create an object to help track time
+	clock = pygame.time.Clock()
 
 	# set the current window caption
 	pygame.display.set_caption("PANDEMIC DELIVERY BOY")
@@ -43,21 +51,20 @@ def main():
 	while True:
 		# get events from the queue
 		for event in pygame.event.get():
-
-			# quit the game
+			# QUIT THE GAME
 			if event.type == QUIT:
 				pygame.quit()
 				sys.exit()
 
 			# MOUSE INPUT
-			if event.type == MOUSEBUTTONDOWN:
-				print("MOUSE DOWN")
+			# if event.type == MOUSEBUTTONDOWN:
+				# print("MOUSE DOWN")
 
-			if event.type == MOUSEMOTION:
-				print("MOUSE MOTION")
+			# if event.type == MOUSEMOTION:
+				# print("MOUSE MOTION")
 
-			if event.type == MOUSEBUTTONUP:
-				print("MOUSE UP")
+			# if event.type == MOUSEBUTTONUP:
+				# print("MOUSE UP")
 
 			# KEYBOARD INPUT (PRESS DOWN)
 			if event.type == KEYDOWN:
@@ -97,11 +104,25 @@ def main():
 			player_pos[1] += 1	
 
 		# ---------------------------------------------------------------------
+		#  UPDATE OBJECTS ATTRIBUTES
+		# ---------------------------------------------------------------------
+		if player_pos[1] > GAME_HEIGHT_SCREEN - player.get_height():
+			player_y_momentum = -player_y_momentum
+		else:
+			player_y_momentum += 0.5
+		
+		player_pos[1] += player_y_momentum
+
+		# ---------------------------------------------------------------------
 		#  CLEAR SCREEN
 		# ---------------------------------------------------------------------
 		screen.fill(COLOR_AQUA_BLUE)
 
+		# ---------------------------------------------------------------------
+		#  DRAW FRAME
+		# ---------------------------------------------------------------------
 		screen.blit(player, player_pos)
+		# screen.blit(text, (5, 10))
 
 		# update portions of the screen for software displays
 		# pygame.display.update()
