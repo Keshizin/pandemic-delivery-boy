8614d7c409bc592d16e4ebffe8db4475472960dd
Keshizin <fab.takeshi@gmail.com>
2020-11-25 17:22:49 -0300

    Inclusão do game over e menu

--- game.py
+++ game.py
@@ -12,18 +12,39 @@ from gameobjects.vector2 import Vector2
 #  GAME - GLOBAL SCOPE
 # -----------------------------------------------------------------------------
 
+# ESTADOS DO JOGO
+# 1 - MENU
+# 2 - GAME
+# 3 - GAME OVER
+GAME_STATE = 2
+
 # sprite = Sprite("teste")
 # sprite.print_name()
-
-# Inicializando todos os módulos de PyGame
-pygame.init()
-
-# Quantidade de quadros por segundo
-FPS = 120
 
 # Resolução da janela do jogo
 WINDOW_WIDTH_SCREEN = 912
 WINDOW_HEIGHT_SCREEN = 576
+
+# Inicializando todos os módulos de PyGame
+pygame.init()
+
+# objeto de tratamento de tempo
+clock = pygame.time.Clock()
+
+# texto da barra de título da janela
+pygame.display.set_caption("PANDEMIC DELIVERY BOY")
+
+# posição da janela na tela
+os.environ['SDL_VIDEO_WINDOW_POS'] = "%d,%d" % (100, 100)
+
+# criação da surface para a janela principal - SURFACE CREATION HERE!
+window_surface = pygame.display.set_mode((WINDOW_WIDTH_SCREEN, WINDOW_HEIGHT_SCREEN), 0, 32)
+
+# criação da surface com o tamanho correto dos sprites (304x192 pixels) - melhor desempenho
+display = pygame.Surface((304, 192))
+
+# Quantidade de quadros por segundo
+FPS = 120
 
 # Constantes para as cores
 AQUA_BLUE_COLOR = (146,244,255)
@@ -39,20 +60,31 @@ keys = [False, False, False, False]
 # -----------------------------------------------------------------------------
 # criação do personagem do jogador (pos_x, pos_y, speed_x, speed_y, imagem)
 player = Sprite(50, 150, 1, 1, 'assets/p1.png')
+
+# criação dos carros
 car1 = Sprite(354, 42, -random.randrange(1, 4), 0, 'assets/car1.png')
 car2 = Sprite(354, 135, -random.randrange(1, 4), 0, 'assets/car2.png')
 car3 = Sprite(354, 39, -random.randrange(1, 4), 0, 'assets/car3.png')
+
+# criação dos contaminadores
+enemy1 = Sprite(60, 100, -random.randrange(1, 2), -random.randrange(1, 2), 'assets/enemy1.png')
+
 
 enemy = pygame.image.load('assets/enemy1.png')
 enemy.set_colorkey((255,255,255))
 enemy_pos = [100, 100]
 enemy_collision_rect = pygame.Rect(enemy_pos[0], enemy_pos[1], 15, 15)
 
+heart_img = pygame.image.load('assets/heart.png')
+heart_img.set_colorkey((255,255,255))
+
 boxo_img = pygame.image.load('assets/boxo.png')
 boxc_img = pygame.image.load('assets/boxc.png')
 barrel_img = pygame.image.load('assets/barrel.png')
 cone_img = pygame.image.load('assets/cone.png')
 cone_img.set_colorkey((255,255,255))
+
+gameover_img = pygame.image.load('assets/gameover.png')
 
 # -----------------------------------------------------------------------------
 #  DECLARAÇÃO DE PERSONAGENS E OBJETOS
@@ -124,6 +156,9 @@ game_map = game_map1
 # (!) identificador da região atual
 current_map_region = 1
 
+# lista de tiles
+collision_tiles_rects = []
+
 # imagens de background de cada região
 bkg_1_img = pygame.image.load('assets/bkg.png')
 bkg_2_img = pygame.image.load('assets/bkg2.png')
@@ -140,29 +175,16 @@ font = pygame.font.Font('assets/pixelart.ttf', 8)
 #  MAIN
 # -----------------------------------------------------------------------------
 def main():
-	print("Pandemic Delivery Game")
-
+	print("Pandemic Delivery Game !!!")
 	global player
-	global current_map_region
 	global game_map
 	global bkgs
+	global keys
 
-	# objeto de tratamento de tempo
-	clock = pygame.time.Clock()
+	global GAME_STATE
+	global collision_tiles_rects
 
-	# texto da barra de título da janela
-	pygame.display.set_caption("PANDEMIC DELIVERY BOY")
-
-	# posição da janela na tela
-	os.environ['SDL_VIDEO_WINDOW_POS'] = "%d,%d" % (100, 100)
-
-	# criação da surface para a janela principal - SURFACE CREATION HERE!
-	window_surface = pygame.display.set_mode((WINDOW_WIDTH_SCREEN, WINDOW_HEIGHT_SCREEN), 0, 32)
-
-	# criação da surface com o tamanho correto dos sprites (304x192 pixels) - melhor desempenho
-	display = pygame.Surface((304, 192))
-
-	# lista de tiles/objetos colisores
+	# lista de tiles
 	collision_tiles_rects = []
 
 	# preenche a lista de tiles/objetos de acordo com a matriz game_map
@@ -219,180 +241,219 @@ def main():
 				if event.key == K_DOWN:
 					keys[3] = False
 
-		# ---------------------------------------------------------------------
-		#  PLAYER INPUT
-		# ---------------------------------------------------------------------
-		# player_movement representa o quanto devemos deslocar o personagem!
-		player_movement = [0, 0]
+		if GAME_STATE == 2:
+			game_main_loop()
+		elif GAME_STATE == 3:
+			game_over_loop()
 
-		# ESQUERDA
-		if keys[0]:
-			player.set_image('assets/p3.png')
-			player_movement[0] = -player.get_speed_x()
+def game_main_loop():
+	global current_map_region
+	global collision_tiles_rects
+	global GAME_STATE
 
-		# DIREITA
-		if keys[1]:			
-			player.set_image('assets/p3.png')
-			player.flip_image(True, False) # flip imagem na horizontal
-			player_movement[0] = player.get_speed_x()
+	# ---------------------------------------------------------------------
+	#  PLAYER INPUT
+	# ---------------------------------------------------------------------
+	# player_movement representa o quanto devemos deslocar o personagem!
+	player_movement = [0, 0]
 
-		# CIMA
-		if keys[2]:
-			player.set_image('assets/p2.png')
-			player_movement[1] = -player.get_speed_y()
+	# ESQUERDA
+	if keys[0]:
+		player.set_image('assets/p3.png')
+		player_movement[0] = -player.get_speed_x()
+
+	# DIREITA
+	if keys[1]:			
+		player.set_image('assets/p3.png')
+		player.flip_image(True, False) # flip imagem na horizontal
+		player_movement[0] = player.get_speed_x()
+
+	# CIMA
+	if keys[2]:
+		player.set_image('assets/p2.png')
+		player_movement[1] = -player.get_speed_y()
+	
+	# EMBAIXO
+	if keys[3]:
+		player.set_image('assets/p1.png')
+		player_movement[1] = player.get_speed_y()
+
+	# ---------------------------------------------------------------------
+	#  ATUALIZANDO A POSIÇÃO DOS OBJETOS
+	# ---------------------------------------------------------------------
+	if current_map_region == 1:
+		car1.update(current_map_region)
+		car2.update(current_map_region)
+
+	elif current_map_region == 3:
+		car3.update(1)
+		# obter o vetor para perseguir o jogador
+		enemy_vector = enemy1.get_move_intention(player)
+
+	# enemy_vector = Vector2(enemy_pos[0], enemy_pos[1])
+	
+	# destination_x = player_pos[0] - player.get_width() / 2.0
+	# destination_y = player_pos[1] - player.get_height() / 2.0
+	# destination = (destination_x, destination_y)
+
+	# heading = Vector2.from_points(enemy_vector, destination)
+	# heading.normalize()
+
+	# time_passed_seconds = FPS / 1000.0
+
+	# distance_moved = time_passed_seconds * 5
+	# enemy_vector += heading * distance_moved 
+
+	# enemy_collision_rect.x += int(enemy_vector.x)
+	# enemy_collision_rect.y += int(enemy_vector.y)
+
+	# print("@debug | enemy_vector.x: " +  str(enemy_vector.x))
+	# print("@debug | enemy_vector.y: " +  str(enemy_vector.y))
+	# enemy_pos[0] = enemy_vector.x
+	# enemy_pos[1] = enemy_vector.y
+
+	# enemy_collision_rect.x = enemy_vector.x
+	# enemy_collision_rect.y = enemy_vector.y
+
+	# ---------------------------------------------------------------------
+	#  TESTE DE COLISÃO COM A BORDA, TILES e INIMIGOS
+	# ---------------------------------------------------------------------
+
+	# teste de colisão com os tiles
+	player_movement[0], new_y = player.test_collision_tiles(player_movement[0], player_movement[1], collision_tiles_rects)
+
+	# teste de colisão com as bordas da janela
+	current_map_region = player.test_collision_border(current_map_region)
+
+	# troca de regiões~
+	if current_map_region == 1:
+		bkgs = bkg_1_img
+		game_map = game_map1
+	elif current_map_region == 2:
+		bkgs = bkg_2_img
+		game_map = game_map2
+	elif current_map_region == 3:
+		bkgs = bkg_3_img
+		game_map = game_map3
+	elif current_map_region == 4:
+		bkgs = bkg_4_img
+		game_map = game_map4
+
+	# ---------------------------------------------------------------------
+	#  TESTE DE COLISÃO COM INIMIGOS
+	# ---------------------------------------------------------------------
+	if current_map_region == 1:
+		player.test_collision_enemy(car1)
+		player.test_collision_enemy(car2)
+	elif current_map_region == 3:
+		player.test_collision_enemy(car3)
+
+	# if player_collision_rect.colliderect(enemy_collision_rect):
+	# 	print("OUCH")
+	# else:
+	# 	print("...")
+
+	# ---------------------------------------------------------------------
+	#  CLEAR SCREEN
+	# ---------------------------------------------------------------------
+
+	# limpa a tela com a cor branca
+	display.fill(WHITE_COLOR)
+	# desenha o background da região atual
+	display.blit(bkgs, (0, 0))
+
+	# ---------------------------------------------------------------------
+	#  DRAW GAME MAP
+	# ---------------------------------------------------------------------
+	collision_tiles_rects = []
+	y = 0
+
+	for layer in game_map:
+		x = 0
+		for tile in layer:
 		
-		# EMBAIXO
-		if keys[3]:
-			player.set_image('assets/p1.png')
-			player_movement[1] = player.get_speed_y()
+			if tile == '2':
+				display.blit(barrel_img, (x * 16, y * 16))
 
-		# ---------------------------------------------------------------------
-		#  ATUALIZANDO A POSIÇÃO DOS OBJETOS
-		# ---------------------------------------------------------------------
-		if current_map_region == 1:
-			car1.update(current_map_region)
-			car2.update(current_map_region)
+			if tile == '3':
+				display.blit(boxc_img, (x * 16, y * 16))
 
-		elif current_map_region == 3:
-			car3.update(1)
+			if tile == '4':
+				display.blit(barrel_img, (x * 16, y * 16))
+				
+			if tile != '0':
+				collision_tiles_rects.append(pygame.Rect(x * 16, y * 16, 16, 16))
 
-		# enemy_vector = Vector2(enemy_pos[0], enemy_pos[1])
-		
-		# destination_x = player_pos[0] - player.get_width() / 2.0
-		# destination_y = player_pos[1] - player.get_height() / 2.0
-		# destination = (destination_x, destination_y)
+			x += 1
+		y += 1
 
-		# heading = Vector2.from_points(enemy_vector, destination)
-		# heading.normalize()
+	# ---------------------------------------------------------------------
+	#  DESENHA PERSNAGENS
+	# ---------------------------------------------------------------------
+	if current_map_region == 1:
+		display.blit(car1.get_image(), (int(car1.get_pos_x()), int(car1.get_pos_y())))
+		display.blit(car2.get_image(), (int(car2.get_pos_x()), int(car2.get_pos_y())))
+	elif current_map_region == 3:
+		display.blit(car3.get_image(), (int(car3.get_pos_x()), int(car3.get_pos_y())))
+		display.blit(enemy1.get_image(), (int(enemy1.get_pos_x()), int(enemy1.get_pos_y())))
 
-		# time_passed_seconds = FPS / 1000.0
+	# if map_region == 3:
+	# 	display.blit(enemy, (enemy_vector.x, enemy_vector.y))
 
-		# distance_moved = time_passed_seconds * 5
-		# enemy_vector += heading * distance_moved 
+	# desenha o personagem DO JOGADOR
+	display.blit(player.get_image(), (int(player.get_pos_x()), int(player.get_pos_y())))
+	# rect = player.get_collision_rect();
+	# pygame.draw.rect(display, RED_COLOR, rect)
 
-		# enemy_collision_rect.x += int(enemy_vector.x)
-		# enemy_collision_rect.y += int(enemy_vector.y)
+	# ---------------------------------------------------------------------
+	#  USER INTERFACE
+	# ---------------------------------------------------------------------
+	# if map_region == 1:
+	# 	textRegion = font.render('LOJAS', True, BLACK_COLOR, AQUA_BLUE_COLOR)
+	# elif map_region == 2:
+	# 	textRegion = font.render('CAMINHO 2', True, BLACK_COLOR, AQUA_BLUE_COLOR)
+	# elif map_region == 3:
+	# 	textRegion = font.render('CAMINHO 1', True, BLACK_COLOR, AQUA_BLUE_COLOR)
+	# elif map_region == 4:
+	# 	textRegion = font.render('CASAS', True, BLACK_COLOR, AQUA_BLUE_COLOR)
 
-		# print("@debug | enemy_vector.x: " +  str(enemy_vector.x))
-		# print("@debug | enemy_vector.y: " +  str(enemy_vector.y))
-		# enemy_pos[0] = enemy_vector.x
-		# enemy_pos[1] = enemy_vector.y
+	# display.blit(textRegion, (10, 10))
 
-		# enemy_collision_rect.x = enemy_vector.x
-		# enemy_collision_rect.y = enemy_vector.y
+	# desenha a vida do jogador
+	x = 10
+	for life in range(player.get_lifes()):
+		display.blit(heart_img, (x, 10))
+		x += 10 + heart_img.get_width()
 
-		# ---------------------------------------------------------------------
-		#  TESTE DE COLISÃO COM A BORDA, TILES e INIMIGOS
-		# ---------------------------------------------------------------------
+	# ---------------------------------------------------------------------
+	#  ATUALIZA O FRAME
+	# ---------------------------------------------------------------------
 
-		# teste de colisão com os tiles
-		player_movement[0], new_y = player.test_collision_tiles(player_movement[0], player_movement[1], collision_tiles_rects)
+	# faz a escala da surface para o tamanho da janela
+	window_surface.blit(pygame.transform.scale(display, (WINDOW_WIDTH_SCREEN, WINDOW_HEIGHT_SCREEN)),(0,0))
+	
+	# atualiza uma porção da janela com a surface
+	# pygame.display.update()
+	# atualiza a Surface para a tela inteira
+	pygame.display.flip()
 
-		# teste de colisão com as bordas da janela
-		current_map_region = player.test_collision_border(current_map_region)
+	clock.tick(FPS)
 
-		# troca de regiões~
-		if current_map_region == 1:
-			bkgs = bkg_1_img
-			game_map = game_map1
-		elif current_map_region == 2:
-			bkgs = bkg_2_img
-			game_map = game_map2
-		elif current_map_region == 3:
-			bkgs = bkg_3_img
-			game_map = game_map3
-		elif current_map_region == 4:
-			bkgs = bkg_4_img
-			game_map = game_map4
+	# ---------------------------------------------------------------------
+	#  END GAME
+	# ---------------------------------------------------------------------
+	if player.get_lifes() == 0:
+		print("GAME OVER")
+		GAME_STATE = 3
+		return 0
 
-		# ---------------------------------------------------------------------
-		#  TESTE DE COLISÃO COM INIMIGOS
-		# ---------------------------------------------------------------------
+def game_over_loop():
+	display.fill(AQUA_BLUE_COLOR)
+	display.blit(gameover_img, (0, 0))
 
-		# if player_collision_rect.colliderect(enemy_collision_rect):
-		# 	print("OUCH")
-		# else:
-		# 	print("...")
-
-		# ---------------------------------------------------------------------
-		#  CLEAR SCREEN
-		# ---------------------------------------------------------------------
-
-		# limpa a tela com a cor branca
-		display.fill(WHITE_COLOR)
-		# desenha o background da região atual
-		display.blit(bkgs, (0, 0))
-
-		# ---------------------------------------------------------------------
-		#  DRAW GAME MAP
-		# ---------------------------------------------------------------------
-		collision_tiles_rects = []
-		y = 0
-
-		for layer in game_map:
-			x = 0
-			for tile in layer:
-			
-				if tile == '2':
-					display.blit(barrel_img, (x * 16, y * 16))
-
-				if tile == '3':
-					display.blit(boxc_img, (x * 16, y * 16))
-
-				if tile == '4':
-					display.blit(barrel_img, (x * 16, y * 16))
-					
-				if tile != '0':
-					collision_tiles_rects.append(pygame.Rect(x * 16, y * 16, 16, 16))
-
-				x += 1
-			y += 1
-
-		# ---------------------------------------------------------------------
-		#  USER INTERFACE
-		# ---------------------------------------------------------------------
-		# if map_region == 1:
-		# 	textRegion = font.render('LOJAS', True, BLACK_COLOR, AQUA_BLUE_COLOR)
-		# elif map_region == 2:
-		# 	textRegion = font.render('CAMINHO 2', True, BLACK_COLOR, AQUA_BLUE_COLOR)
-		# elif map_region == 3:
-		# 	textRegion = font.render('CAMINHO 1', True, BLACK_COLOR, AQUA_BLUE_COLOR)
-		# elif map_region == 4:
-		# 	textRegion = font.render('CASAS', True, BLACK_COLOR, AQUA_BLUE_COLOR)
-
-		# display.blit(textRegion, (10, 10))
-
-		# ---------------------------------------------------------------------
-		#  DESENHA PERSNAGENS
-		# ---------------------------------------------------------------------
-		if current_map_region == 1:
-			display.blit(car1.get_image(), (int(car1.get_pos_x()), int(car1.get_pos_y())))
-			display.blit(car2.get_image(), (int(car2.get_pos_x()), int(car2.get_pos_y())))
-		elif current_map_region == 3:
-			display.blit(car3.get_image(), (int(car3.get_pos_x()), int(car3.get_pos_y())))
-
-
-		# if map_region == 3:
-		# 	display.blit(enemy, (enemy_vector.x, enemy_vector.y))
-
-		# desenha o personagem DO JOGADOR
-		display.blit(player.get_image(), (int(player.get_pos_x()), int(player.get_pos_y())))
-		# rect = player.get_collision_rect();
-		# pygame.draw.rect(display, RED_COLOR, rect)
-
-		# ---------------------------------------------------------------------
-		#  ATUALIZA O FRAME
-		# ---------------------------------------------------------------------
-
-		# faz a escala da surface para o tamanho da janela
-		window_surface.blit(pygame.transform.scale(display, (WINDOW_WIDTH_SCREEN, WINDOW_HEIGHT_SCREEN)),(0,0))
-		
-		# atualiza uma porção da janela com a surface
-		# pygame.display.update()
-		# atualiza a Surface para a tela inteira
-		pygame.display.flip()
-
-		clock.tick(FPS)
+	window_surface.blit(pygame.transform.scale(display, (WINDOW_WIDTH_SCREEN, WINDOW_HEIGHT_SCREEN)),(0,0))
+	pygame.display.flip()
+	clock.tick(FPS)
 
 if __name__ == '__main__':
 	main()
--- gameobjects/sprite.py
+++ gameobjects/sprite.py
@@ -1,6 +1,7 @@
 # Pandemic Delivery Game
 # This code is released under MIT by Fabio Ishikawa
 import pygame, random
+from gameobjects.vector2 import Vector2
 
 # -------------------------------------------------------------------------
 #  CLASSE - SPRITE DE UM PERSONAGEM
@@ -17,7 +18,8 @@ class Sprite(object):
 		self.width = self.image.get_width()
 		self.height = self.image.get_height()
 		self.collision_rect = pygame.Rect(self.pos_x, self.pos_y, self.width, self.height)
-		self.lifes
+		self.lifes = 6
+		self.safe = False
 
 	def set_image(self, filename):
 		self.image = pygame.image.load(filename)
@@ -35,6 +37,12 @@ class Sprite(object):
 	def get_pos_y(self):
 		return self.pos_y
 
+	def get_width(self):
+		return self.width
+
+	def get_height(self):
+		return self.height
+
 	def get_speed_x(self):
 		return self.speed_x
 
@@ -43,6 +51,15 @@ class Sprite(object):
 
 	def get_image(self):
 		return self.image
+
+	def get_lifes(self):
+		return self.lifes
+
+	def set_safe(self, safe):
+		self.safe = safe
+
+	def get_safe(self):
+		return self.safe
 
 	def set_collision_rect_pos(self, x, y):
 		self.collision_rect.x = x
@@ -54,11 +71,39 @@ class Sprite(object):
 	def update(self, map_region):
 		self.pos_x += self.speed_x
 		self.pos_y += self.speed_y
+		self.collision_rect.x = self.pos_x
+		self.collision_rect.y = self.pos_y
 
 		if map_region == 1:
 			if self.pos_x + self.width < 0:
 				self.pos_x = 304 + self.width
+				self.collision_rect.x = self.pos_x
 				self.speed_x = -random.randrange(1, 4)
+		# elif map_region == 3:
+
+
+	def get_move_intention(self, destiny):
+			pos_vector = Vector2(self.pos_x, self.pos_y)
+
+			destination_x = destiny.get_pos_x() - destiny.get_width() / 2.0
+			destination_y = destiny.get_pos_y() - destiny.get_height() / 2.0
+			destination = (destination_x, destination_y)
+
+			heading = Vector2.from_points(pos_vector, destination)
+			heading.normalize()
+
+			time_passed_seconds = 60 / 1000.0
+
+			distance_moved = time_passed_seconds * 5
+			pos_vector += heading * distance_moved 
+
+			self.collision_rect.x += int(pos_vector.x)
+			self.collision_rect.y += int(pos_vector.y)
+
+			self.pos_x = pos_vector.x
+			self.pos_y = pos_vector.y
+			self.collision_rect.x = pos_vector.x
+			self.collision_rect.y = pos_vector.y
 
 	def test_collision_border(self, map_region):
 		# -------------------------------------------------------------------------
@@ -155,3 +200,12 @@ class Sprite(object):
 		self.pos_y = self.collision_rect.y
 
 		return new_x, new_y
+
+	def test_collision_enemy(self, enemy):
+		if self.collision_rect.colliderect(enemy.get_collision_rect()):
+			if enemy.get_safe() == False:
+				self.lifes -= 1
+				enemy.set_safe(True)
+		else:
+			if enemy.get_safe() == True:
+				enemy.set_safe(False)
